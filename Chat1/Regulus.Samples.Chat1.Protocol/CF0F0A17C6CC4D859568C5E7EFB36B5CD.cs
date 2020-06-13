ff754a02f8850e29418d374d8cd9f9db1da56e58
jc <jiowchern@gmail.com>
2020-06-13 21:02:28 +0800

    update readme

--- Chat1/Regulus.Samples.Chat1.Protocol/CF0F0A17C6CC4D859568C5E7EFB36B5CD.cs
+++ Chat1/Regulus.Samples.Chat1.Protocol/CF0F0A17C6CC4D859568C5E7EFB36B5CD.cs
@@ -3,13 +3,13 @@
             using System.Collections.Generic;
             
             
-                public class C75969740C825E5FF1A42D4203C845E0C : Regulus.Remote.IProtocol
+                public class CF0F0A17C6CC4D859568C5E7EFB36B5CD : Regulus.Remote.IProtocol
                 {
                     Regulus.Remote.InterfaceProvider _InterfaceProvider;
                     Regulus.Remote.EventProvider _EventProvider;
                     Regulus.Remote.MemberMap _MemberMap;
                     Regulus.Serialization.ISerializer _Serializer;
-                    public C75969740C825E5FF1A42D4203C845E0C()
+                    public CF0F0A17C6CC4D859568C5E7EFB36B5CD()
                     {
                         var types = new Dictionary<Type, Type>();
                         types.Add(typeof(Regulus.Samples.Chat1.Common.IBroadcastable) , typeof(Regulus.Samples.Chat1.Common.Ghost.CIBroadcastable) );
@@ -20,13 +20,13 @@ types.Add(typeof(Regulus.Samples.Chat1.Common.IChatable) , typeof(Regulus.Sample
                         eventClosures.Add(new Regulus.Samples.Chat1.Common.Invoker.IBroadcastable.MessageEvent() );
                         _EventProvider = new Regulus.Remote.EventProvider(eventClosures);
 
-                        _Serializer = new Regulus.Serialization.Serializer(new Regulus.Serialization.DescriberBuilder(typeof(Regulus.Remote.ClientToServerOpCode),typeof(Regulus.Remote.PackageCallMethod),typeof(Regulus.Remote.PackageErrorMethod),typeof(Regulus.Remote.PackageInvokeEvent),typeof(Regulus.Remote.PackageLoadSoul),typeof(Regulus.Remote.PackageLoadSoulCompile),typeof(Regulus.Remote.PackageProtocolSubmit),typeof(Regulus.Remote.PackageRelease),typeof(Regulus.Remote.PackageReturnValue),typeof(Regulus.Remote.PackageUnloadSoul),typeof(Regulus.Remote.PackageUpdateProperty),typeof(Regulus.Remote.RequestPackage),typeof(Regulus.Remote.ResponsePackage),typeof(Regulus.Remote.ServerToClientOpCode),typeof(System.Boolean),typeof(System.Byte[]),typeof(System.Byte[][]),typeof(System.Char),typeof(System.Char[]),typeof(System.Guid),typeof(System.Int32),typeof(System.String)).Describers);
+                        _Serializer = new Regulus.Serialization.Serializer(new Regulus.Serialization.DescriberBuilder(typeof(Regulus.Remote.ClientToServerOpCode),typeof(Regulus.Remote.PackageCallMethod),typeof(Regulus.Remote.PackageErrorMethod),typeof(Regulus.Remote.PackageInvokeEvent),typeof(Regulus.Remote.PackageLoadSoul),typeof(Regulus.Remote.PackageLoadSoulCompile),typeof(Regulus.Remote.PackageProtocolSubmit),typeof(Regulus.Remote.PackageRelease),typeof(Regulus.Remote.PackageReturnValue),typeof(Regulus.Remote.PackageSetProperty),typeof(Regulus.Remote.PackageSetPropertyDone),typeof(Regulus.Remote.PackageUnloadSoul),typeof(Regulus.Remote.PackageUpdateProperty),typeof(Regulus.Remote.RequestPackage),typeof(Regulus.Remote.ResponsePackage),typeof(Regulus.Remote.ServerToClientOpCode),typeof(System.Boolean),typeof(System.Byte[]),typeof(System.Byte[][]),typeof(System.Char),typeof(System.Char[]),typeof(System.Guid),typeof(System.Int32),typeof(System.String)).Describers);
 
 
                         _MemberMap = new Regulus.Remote.MemberMap(new System.Reflection.MethodInfo[] {new Regulus.Utility.Reflection.TypeMethodCatcher((System.Linq.Expressions.Expression<System.Action<Regulus.Samples.Chat1.Common.IChatable,System.String,System.String>>)((ins,_1,_2) => ins.Send(_1,_2))).Method} ,new System.Reflection.EventInfo[]{ typeof(Regulus.Samples.Chat1.Common.IBroadcastable).GetEvent("MessageEvent") }, new System.Reflection.PropertyInfo[] { }, new System.Tuple<System.Type, System.Func<Regulus.Remote.IProvider>>[] {new System.Tuple<System.Type, System.Func<Regulus.Remote.IProvider>>(typeof(Regulus.Samples.Chat1.Common.IBroadcastable),()=>new Regulus.Remote.TProvider<Regulus.Samples.Chat1.Common.IBroadcastable>()),new System.Tuple<System.Type, System.Func<Regulus.Remote.IProvider>>(typeof(Regulus.Samples.Chat1.Common.IChatable),()=>new Regulus.Remote.TProvider<Regulus.Samples.Chat1.Common.IChatable>())});
                     }
 
-                    byte[] Regulus.Remote.IProtocol.VerificationCode { get { return new byte[]{117,150,151,64,200,37,229,255,26,66,212,32,60,132,94,12};} }
+                    byte[] Regulus.Remote.IProtocol.VerificationCode { get { return new byte[]{240,240,161,124,108,196,216,89,86,140,94,126,251,54,181,205};} }
                     Regulus.Remote.InterfaceProvider Regulus.Remote.IProtocol.GetInterfaceProvider()
                     {
                         return _InterfaceProvider;
--- Chat1/readme.md
+++ Chat1/readme.md
@@ -1,30 +1,17 @@
-# 教學說明
-以下範例為介紹如何建立起一個聊天室專案。
-## 功能開發
-### 1.引入程式庫  
+# Sample
+The following example shows how to create a chat project.
+## Development environment
+Import library  
 ![程式庫](./doc/lib-refs.jpg)  
-### 2.建立主專案檔案結構  
+Project structure 
 ![檔案路徑](./doc/files-struct.jpg)  
-Common - Client 與 Server 的共通定義。  
-Main - 聊天室的主要邏輯。   
-Protocol - 空專案，目的是產生共通物件定義的實作。  
-## 建立 Protocol
-此步驟為建立 Client 與 Server 之間協定程式碼。 執行期需要它。  
-使用 **Regulus.Application.Protocol.CodeWriter** 產生程式碼到 Regulus.Samples.Chat1.Protocol 。  
-建立 Directory.Build.targets 內容如下。  
-``` xml
-<Project>
-    <Target Condition="'$(ProjectName)'=='Regulus.Samples.Chat1.Common' And '$(SolutionDir)'!='*Undefined*'" Name="CreateProtocol" BeforeTargets="Build">
-        <Exec Command="del $(SolutionDir)Chat1\Regulus.Samples.Chat1.Protocol\*.cs /q"/>
-        <Exec Command="dotnet run --project $(SolutionDir)/Regulus/Regulus.Application.Protocol.CodeWriter  --common $(TargetPath) --output $(SolutionDir)\Chat1\Regulus.Samples.Chat1.Protocol" />
-    </Target>
-</Project>
-```  
-之後當編譯 **Regulus.Samples.Chat1.Common** 時會自動產生程式碼。  
-![檔案路徑](./doc/protocol-codes.jpg)  
 
+## Create Protocol
+```
+dotnet run --project $(SolutionDir)/Regulus/Regulus.Application.Protocol.CodeWriter  --common $(TargetPath) --output $(SolutionDir)\Chat1\Regulus.Samples.Chat1.Protocol
+```
 
-## Console 執行
+## Run Console 
 Client  
 ```
 C:\Regulus.Samples> dotnet run --project .\Regulus\Regulus.Application.Client --protocol .\Chat1\Regulus.Samples.Chat1.Protocol\bin\Debug\netstandard2.0\Regulus.Samples.Chat1.Protocol.dll
@@ -34,16 +21,16 @@ Server
 C:\Regulus.Samples> dotnet run --project .\Regulus\Regulus.Application.Server --port 53771 --protocol .\Chat1\Regulus.Samples.Chat1.Protocol\bin\Debug\netstandard2.0\Regulus.Samples.Chat1.Protocol.dll --entryname Regulus.Samples.Chat1.Main.Room --entry .\Chat1\Regulus.Samples.Chat1.Main\bin\Debug\netstandard2.0\Regulus.Samples.Chat1.Main.dll
 ```
 
-## Unity Client
+## In Unity
 
-### 1.環境佈署
+Import library
 ```
-必要開發套件
+Essential library
 C:\Regulus.Samples\Chat1\Regulus.Samples.Chat1.Unity> dotnet publish ../../Regulus/Regulus.Remote.Client -o ./Assets/Project/Server.Library
 Chat1's Protocol
 C:\Regulus.Samples\Chat1\Regulus.Samples.Chat1.Unity> dotnet publish ../Regulus.Samples.Chat1.Protocol -o ./Assets/Project/Server.Library
 ```
-### 2.Create Agent
+Create Agent
 ```csharp
 // Regulus.Samples\Chat1\Regulus.Samples.Chat1.Unity\Assets\Project\Scripts\Chat1Agent.cs
 public class Chat1Agent : MonoBehaviour
@@ -61,7 +48,7 @@ public class Chat1Agent : MonoBehaviour
 
 ```
 
-### 3.Get Connect
+Get Connect
 ```csharp
 // Regulus.Samples\Chat1\Regulus.Samples.Chat1.Unity\Assets\Project\Scripts\Chat1Connect.cs
 public class Chat1Connect : MonoBehaviour
@@ -97,7 +84,7 @@ public class Chat1Connect : MonoBehaviour
     }
 }
 ```
-### 4.Receive Message
+Receive Message
 ```csharp
 // Regulus.Samples\Chat1\Regulus.Samples.Chat1.Unity\Assets\Project\Scripts\Chat1MessageView.cs
 public class Chat1MessageView : MonoBehaviour
@@ -133,7 +120,7 @@ public class Chat1MessageView : MonoBehaviour
     }
 }
 ```
-### 5.Send Message
+Send Message
 ```csharp
 // Regulus.Samples\Chat1\Regulus.Samples.Chat1.Unity\Assets\Project\Scripts\Chat1MessageSend.cs
 public class Chat1MessageSend : MonoBehaviour
