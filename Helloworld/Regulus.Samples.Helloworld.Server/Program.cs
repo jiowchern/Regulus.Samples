0813c62945de6e8ac8eb417851e700f58320213c
jc <jiowchern@gmail.com>
2020-08-10 13:01:05 +0800

    update lib.

--- Helloworld/Regulus.Samples.Helloworld.Server/Program.cs
+++ Helloworld/Regulus.Samples.Helloworld.Server/Program.cs
@@ -13,15 +13,16 @@ namespace Regulus.Samples.Helloworld.Server
             var protocol = Regulus.Remote.Protocol.ProtocolProvider.Create(protocolAsm);
 
             var entry = new Entry();
-            
-            var service = Regulus.Remote.Server.Provider.CreateTcp(Regulus.Remote.Server.Provider.CreateService(entry , protocol));
-            service.Bind(port);
+
+            var service = Regulus.Remote.Server.Provider.CreateService(entry, protocol);
+            var listener = Regulus.Remote.Server.Provider.CreateTcp(service);
+            listener.Bind(port);
             System.Console.WriteLine($"start.");
             while (entry.Enable)
             {
                 System.Threading.Thread.Sleep(0);
             }
-            service.Close();
+            listener.Close();
             System.Console.WriteLine($"Press any key to end.");
             System.Console.ReadKey();
         }
